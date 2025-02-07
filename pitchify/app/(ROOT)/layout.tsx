a6d5d0ac5c5a680b2c62b0e29a6ea62fe078f473
KaranNegi0102 <karannegi0102@gmail.com>
2025-02-07 23:36:23 +0530

    changes

--- pitchify/app/(ROOT)/layout.tsx
+++ pitchify/app/(ROOT)/layout.tsx
@@ -1,6 +1,9 @@
+import NavBar from "../components/NavBar";
+
 export function Layout({ children }: Readonly<{children : React.ReactNode}>){
   return (
     <main >
+      <NavBar />
       {children}
     </main>
   )
---
+++ pitchify/app/components/NavBar.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { auth, signOut, signIn } from '@/auth';
+
+const NavBar = async () => {
+  const session = await auth();
+
+  return (
+    <header className="flex items-center justify-between bg-white p-4 shadow-md">
+      <nav className="flex items-center gap-6">
+        <Link href="/">
+          <Image src="/logo.jpg" alt="Vercel Logo" width={70} height={20} />
+        </Link>
+      </nav>
+
+      <div className="flex items-center gap-4">
+        {session?.user ? (
+          <>
+            <Link href="/startup/create">
+              <span className="cursor-pointer text-black">CreateStartup</span>
+            </Link>
+            <form action={async ()=> {
+              "use server"
+              await signOut({redirectTo:"/"})
+              }} 
+              className="cursor-pointer text-black">
+              <button
+              type="submit"
+              >Logout</button>
+            </form>
+            <Link href={`/user/${session.user.id}`}>
+              <span className="cursor-pointer text-black">{session.user.name}</span>
+            </Link>
+          </>
+        ) : (
+          <form action={async() => {
+            "use server"
+            await signIn('github')
+          }
+          } className="cursor-pointer  text-black">
+            <button
+            type="submit"
+            >Login</button>
+          </form>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
