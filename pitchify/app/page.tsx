849abbff68c8118defeca536790db892b707e231
KaranNegi0102 <karannegi0102@gmail.com>
2025-02-18 23:21:43 +0530

    auth done

--- pitchify/app/page.tsx
+++ pitchify/app/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth"
 import SearchForm from "../components/SearchForm"
 import StartupCard,{StartupTypeCard} from "../components/StartupCard"
 // import { client } from "../sanity/lib/client"
@@ -11,6 +12,9 @@ export default async function Home({ searchParams }: {
   const query = (await searchParams).query;
 
   const params = {search:query || null};
+
+  const session = await auth();
+  console.log(session?.id);
 
 
   // const posts = await client.fetch(STARTUPS_QUERY); //used to fetch the post
