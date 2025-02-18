3cc2490a9d91190ffdec43700add623f974fb5fe
KaranNegi0102 <karannegi0102@gmail.com>
2025-02-18 19:55:30 +0530

    vhanges

--- pitchify/components/View.tsx
+++ pitchify/components/View.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import Ping from '@/components/Ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
+
 const View = async ({id}:{id:string}) => {
   const {views: totalViews} = await client.withConfig({useCdn:false}).fetch(STARTUP_VIEWS_QUERY,{id})
 
-  // TODO : update the number of the views as soon as someone views the startup
+  await writeClient
+  .patch(id) //patch is used for updating
+  .set({views:totalViews+1})
+  .commit();
+
+   
   
   return (
     <div className='view-container'>
